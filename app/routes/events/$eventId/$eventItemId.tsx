0406ae01489553605add1884658bb0350c64d81a
ChrisKLoveless <chriskloveless@gmail.com>
2023-04-10 16:40:26 -0700

    WIP: note is undefined on eventItem route

--- app/routes/events/$eventId/$eventItemId.tsx
+++ app/routes/events/$eventId/$eventItemId.tsx
@@ -1,24 +1,23 @@
 import type { ActionArgs, LoaderFunction } from "@remix-run/node";
-import { Form, Link, useLoaderData } from "@remix-run/react";
+import { Form, Link, Outlet, useLoaderData } from "@remix-run/react";
 import { json, redirect } from "@remix-run/node";
 import { getEventItem, deleteEventItem } from "~/models/eventItems.server";
 import { requireUserId } from "~/session.server";
 import invariant from "tiny-invariant";
 
-
 export const loader: LoaderFunction = async ({ params }) => {
   const { eventItemId } = params;
-  
+
   if (!eventItemId) {
-    throw new Response("Uh Oh! There was no id found.", {status: 404})
+    throw new Response("Uh Oh! There was no id found.", { status: 404 });
   }
   const eventItem = await getEventItem(eventItemId);
   if (!eventItem) {
-    throw new Response("Uh Oh! No event item found.", {status: 404});
+    throw new Response("Uh Oh! No event item found.", { status: 404 });
   }
-  console.log({eventItem});
+  console.log({ eventItem });
   return json({ eventItem });
-}
+};
 
 export async function action({ request, params }: ActionArgs) {
   const userId = await requireUserId(request);
@@ -26,7 +25,7 @@ export async function action({ request, params }: ActionArgs) {
 
   const eventId = params.eventId;
   if (!eventId) {
-    throw new Response("Uh Oh! There was no id found.", {status: 404})
+    throw new Response("Uh Oh! There was no id found.", { status: 404 });
   }
   await deleteEventItem({ id: params.eventItemId });
   return redirect(`/events/${eventId}`);
@@ -40,11 +39,23 @@ export default function EventItem() {
       <h4>{data.eventItem.name}</h4>
       <p>{data.eventItem.note}</p>
       <Form method="post">
-        <button type="submit" className="rounded bg-blue-500  px-4 py-2 text-white hover:bg-blue-600 focus:bg-blue-400">
-          Delete
+        <button
+          type="submit"
+          className="rounded bg-blue-500  px-4 py-2 text-white hover:bg-blue-600 focus:bg-blue-400"
+        >
+          Delete Item
         </button>
       </Form>
+      <Link to={"updateEventItem"}>
+        <button
+          type="submit"
+          className="rounded bg-blue-500  px-4 py-2 text-white hover:bg-blue-600 focus:bg-blue-400"
+        >
+          Update Item
+        </button>
+      </Link>
       <Link to={`/events/${data.eventItem.eventId}`}>Back to Event List</Link>
+      <Outlet/>
     </div>
-  )
+  );
 }
