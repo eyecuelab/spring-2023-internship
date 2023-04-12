09e209ae7113d9ce4185698825a81ad95b744fba
ChrisKLoveless <chriskloveless@gmail.com>
2023-04-12 15:55:00 -0700

    take out theme provider

--- app/root.tsx
+++ app/root.tsx
@@ -3,11 +3,6 @@ import { json } from "@remix-run/node";
 
 import type { LoaderArgs } from "@remix-run/node";
 import { getUser } from "./session.server";
-import { ThemeProvider, createTheme } from "@mui/material/styles";
-
-const theme = createTheme({
-  // Override or create new styles, colors, palettes...
-});
 
 export const loader = async ({ request }: LoaderArgs) => {
   return json({ user: await getUser(request) });
@@ -15,17 +10,15 @@ export const loader = async ({ request }: LoaderArgs) => {
 
 export default function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <html lang="en">
-        <head>
-          <meta charSet="utf-8" />
-          <title>GeTogether</title>
-        </head>
-        <body>
-          <Outlet />
-          <LiveReload />
-        </body>
-      </html>
-    </ThemeProvider>
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <title>GeTogether</title>
+      </head>
+      <body>
+        <Outlet />
+        <LiveReload />
+      </body>
+    </html>
   );
 }
